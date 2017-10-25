2869c0a2f9f07d429a16546970d87e37163bcc71
John McIntosh II <jmci10@hotmail.com>
2017-10-25 01:38:34 +0000

    add calculator variables

--- js/functionality.js
+++ js/functionality.js
@@ -1,3 +1,18 @@
+var operationResult = "";
+var actualOperation = "";
+var userInput = "";
+var arrayInput = [];
+var display = "";
+var stringOperation = "";
+var operationObjects = {
+  "dot" : ".",
+  "percent" : "%",
+  "mutiply" : "*",
+  "divide" : "/",
+  "minus" : "-",
+  "plus" : "+"
+}
+
 $(document).ready(function() {
   
 });
